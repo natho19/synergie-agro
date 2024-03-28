fa24d1ef24a6b296df3f1a71d73631ba91d6c3a3
Nathan Akpawu <nakpawu@gmail.com>
2024-03-28 17:01:41 +0000

    page 404

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<?php get_template_part('parts/breadcrumb'); ?>
+
+<!-- Start 404 
+============================================= -->
+<div class="error-page-area default-padding text-center bg-cover">
+    <!-- Shape -->
+    <div class="shape-left" style="background: url(<?= SA_IMG_URL . 'shape/44-left.png'; ?>);"></div>
+    <div class="shape-right" style="background: url(<?= SA_IMG_URL . 'shape/44-right.png'; ?>);"></div>
+    <!-- End Shape -->
+    <div class="container">
+        <div class="error-box">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h1>404</h1>
+                    <h2>Désolé, nous ne trouvons pas cette page !</h2>
+                    <p>La page que vous recherchez a été déplacée, supprimée, renommée ou n'a jamais existé.</p>
+                    <a class="btn mt-20 btn-md btn-theme" href="<?= esc_url(get_home_url()); ?>">Accueil</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End 404 -->
+
+<?php get_footer(); ?>
--- parts/breadcrumb.php
+++ parts/breadcrumb.php
@@ -1,15 +1,24 @@
 <!-- Start Breadcrumb 
 ============================================= -->
+<?php 
+    $home_title = get_the_title(get_option('page_on_front'));
+    $page_title = "";
+    if (is_404()) {
+        $page_title = "404";
+    } else {
+        $page_title = get_the_title();
+    }
+?>
+
 <div class="breadcrumb-area text-center shadow dark-hard bg-cover text-light" style="background-image: url(<?= SA_IMG_URL . '1900x400.png'; ?>);">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 offset-lg-2">
-                <h1><?php the_title(); ?></h1>
+                <h1><?= $page_title; ?></h1>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <?php $home_title = get_the_title(get_option('page_on_front')); ?>
                         <li><a href="<?= esc_url(get_home_url()); ?>"><i class="fas fa-home"></i> <?= $home_title; ?></a></li>
-                        <li class="active"><?php the_title(); ?></li>
+                        <li class="active"><?= $page_title; ?></li>
                     </ol>
                 </nav>
             </div>
