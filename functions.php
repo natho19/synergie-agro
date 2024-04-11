e5d1748b5a0858259a04ee59fc0265a99c0d7641
Nathan Akpawu <nakpawu@gmail.com>
2024-04-11 09:43:15 +0000

    acf initialisation

--- functions.php
+++ functions.php
@@ -3,3 +3,4 @@ require (dirname(__FILE__)) . '/inc/enqueue.php';
 require (dirname(__FILE__)) . '/inc/functions.php';
 require (dirname(__FILE__)) . '/inc/cpt.php';
 require (dirname(__FILE__)) . '/inc/taxonomy.php';
+require (dirname(__FILE__)) . '/inc/acf.php';
---
+++ inc/acf.php
@@ -0,0 +1,20 @@
+<?php
+
+function acf_settings_path($path)
+{
+    $path = get_stylesheet_directory() . '/inc/third-party/acf/';
+    return $path;
+}
+add_filter('acf/settings/path', 'acf_settings_path');
+
+function acf_settings_dir($dir)
+{
+    $dir = get_stylesheet_directory_uri() . '/inc/third-party/acf/';
+    return $dir;
+}
+add_filter('acf/settings/dir', 'acf_settings_dir');
+
+include_once(get_stylesheet_directory() . '/inc/third-party/acf/acf.php');
+
+// Disable ACF in back office
+// add_filter('acf/settings/show_admin', '__return_false');
