2909f9c4081d0a0bec4537f8f45e274be58bd6fb
Nathan Akpawu <nakpawu@gmail.com>
2024-05-24 11:05:27 +0000

    translation part four

--- functions.php
+++ functions.php
@@ -5,3 +5,4 @@ require (dirname(__FILE__)) . '/inc/cpt.php';
 require (dirname(__FILE__)) . '/inc/taxonomy.php';
 require (dirname(__FILE__)) . '/inc/acf.php';
 require (dirname(__FILE__)) . '/inc/third-party/admin/admin-init.php';
+require (dirname(__FILE__)) . '/inc/third-party/class-wp-bootstrap-navwalker.php';
--- header.php
+++ header.php
@@ -71,21 +71,16 @@
                         <i class="fa fa-times"></i>
                     </button>
 
-                    <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                        <li><a href="http://localhost:81/synergie-agro/" class="active">Accueil</a></li>
-                        <li><a href="http://localhost:81/synergie-agro/a-propos/">A propos</a></li>
-                        <li><a href="http://localhost:81/synergie-agro/domaines-dintervention/">Domaines d'intervention</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projets</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="http://localhost:81/synergie-agro/statuts/realisations/">Réalisations</a></li>
-                                <li><a href="http://localhost:81/synergie-agro/statuts/projets-futurs/">Projets futurs</a></li>
-                                <li><a href="http://localhost:81/synergie-agro/projets/">Tous nos projets</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="http://localhost:81/synergie-agro/actualites/">Actualités</a></li>
-                        <li><a href="http://localhost:81/synergie-agro/contact/">Contact</a></li>
-                    </ul>
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'main',
+                        'container' => false,
+                        'fallback_cb' => '__return_false',
+                        'items_wrap' => '<ul id="%1$s" class="%2$s nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">%3$s</ul>',
+                        'depth' => 2,
+                        'walker' => new bootstrap_5_wp_nav_menu_walker()
+                    ));
+                    ?>
 
                     <div class="select-dropdown">
                         <i class="fas fa-globe"></i>
